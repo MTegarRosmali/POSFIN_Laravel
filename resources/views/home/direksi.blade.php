2c7da2c65cf2866aabfd01610d070e10fdef09e4
MTegarRosmali <02tegarrosmali@gmail.com>
2023-09-18 20:47:19 +0700

    mengubah logo

--- resources/views/home/direksi.blade.php
+++ resources/views/home/direksi.blade.php
@@ -22,7 +22,7 @@
   <!-- navbar -->
   <nav class="navbar navbar-expand-lg navbar-light bg-white shadow shadow-sm fixed-top fy-3">
     <div class="container">
-      <a class="navbar-brand fw-bold" href="#"><span class="primary">POS</span>FIN</a>
+      <a class="navbar-brand fw-bold" href="/"><span class="primary">POS</span>FIN</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
